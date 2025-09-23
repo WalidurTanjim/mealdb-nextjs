56289ac30aa923943a5990cd2bbe764d92b41073
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-23 11:47:24 +0600

    .env file and dbConnect file created

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,17 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const dbConnect = async (collection_name) => {
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    return client.db(process.env.DB_NAME).collection(collection_name)
+}
+
+export default dbConnect;
