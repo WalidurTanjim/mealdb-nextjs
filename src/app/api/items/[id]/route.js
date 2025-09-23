0f349af22f3e2c151eb181d97c418e9e537bde21
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-23 12:20:17 +0600

    patch & delete data by id api route is working well

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -13,14 +13,25 @@ export async function GET(req, { params }) {
 
 // delete single data api route
 export async function DELETE(req, { params }) {
-    const idParams = await params;
+    const idParams = (await params)?.id;
+    const query = { _id: new ObjectId(idParams) };
+    const db = await dbConnect('users');
+    const result = await db.deleteOne(query);
  
-    return Response.json({ idParams })
+    return Response.json({ result })
 }
 
 // patch single data api route
 export async function PATCH(req, { params }) {
-    const idParams = await params;
+    const idParams = (await params)?.id;
+    const postedData = await req.json();
+    const query = { _id: new ObjectId(idParams) };
+    const db = await dbConnect('users');
+    const filter = {
+        $set: {...postedData}
+    }
+    const options = { upsert: true };
+    const result = await db.updateOne(query, filter, options);
  
-    return Response.json({ idParams })
+    return Response.json({ result })
 }
